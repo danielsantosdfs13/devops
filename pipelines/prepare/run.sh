68d9462a7028c84d07dc2b4fa450b0c86c82769d
Daniel Santos <daniel.santos@bancointer.com.br>
2020-09-16 09:49:42 -0300

    fix tfvars

--- pipelines/prepare/run.sh
+++ pipelines/prepare/run.sh
@@ -1,3 +1,3 @@
 terraform init ./prepare/terraform
-terraform plan -var-file="inputs/terraform.tfvars" ./prepare/terraform 
+terraform plan -var-file="pipelines/inputs/terraform.tfvars" ./prepare/terraform 
 # terraform apply --auto-approve
