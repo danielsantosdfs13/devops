3250cb805ed96731f394a9b4200192f50642e90d
Daniel Santos <daniel.santos@bancointer.com.br>
2020-09-16 22:21:18 -0300

    fix

--- application/backend-foo/server.js
+++ application/backend-foo/server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();app.listen(8080, () => {
- console.log("Server running on port 3000");
+ console.log("Server running on port 8080");
 });
 
 app.get("/api/", (req, res, next) => {
